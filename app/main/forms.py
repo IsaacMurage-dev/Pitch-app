e23b715dd9de24f728af3c07cb71dcced9a5923b
Your Name <you@example.com>
2021-09-21 22:23:31 +0300

    modify forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import Required
 
 
 class PitchForm(FlaskForm):
-    pitch = TextAreaField('Generate your Pitch', validators=[Required()])
+    pitch = TextAreaField('Create your Pitch', validators=[Required()])
     my_category = SelectField('Genre', choices=[('Inspirational-Stories', 'Inspirational Stories'), ('Technology', 'Technology'), ('Entrepreneurship', 'Entrepreneurship'), (
         'Cars-Enthusiasim', 'Cars Enthusiasim'), ('Pun-Talent-Sports-Entertainment', 'Pun,Talent,Sports and Entertainment')], validators=[Required()])
     submit = SubmitField('Pitch It!')
@@ -16,6 +16,6 @@ class CommentForm(FlaskForm):
 
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Write something about yourself',
+    bio = TextAreaField('Give a brief description about yourself',
                         validators=[Required()])
     submit = SubmitField('Submit')
