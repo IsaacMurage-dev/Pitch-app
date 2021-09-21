484e71803356447db229540d5274516caace67ae
Your Name <you@example.com>
2021-09-21 23:19:32 +0300

    make changes app__init__

--- app/__init__.py
+++ app/__init__.py
@@ -23,11 +23,10 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
-    # app configurations
     app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
 
-    # Initializing flask extensions
+    # Initialize flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
